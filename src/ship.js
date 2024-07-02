f44074d03b2c2c4cdb0cb6e07d9e406504db55a9
Eduar <donsecun@gmail.com>
2024-07-02 16:09:49 +0200

    improving board function: placing cell of two cell's ships;

--- src/ship.js
+++ src/ship.js
@@ -27,7 +27,7 @@ class gamer {
 function boardship (){ // create the board with numbers and letters 
  var a = 0; // var to enumerate the first row
  var b = 0; // var to create the id cell
- var ascii = 64;
+ var ascii = 64; // var to put letters in the first column
  var boardshipcont = document.createElement('table');
  for (var i = 0; i < 11; i++) {
     var tr = document.createElement('tr'); // Create a row
@@ -38,10 +38,12 @@ function boardship (){ // create the board with numbers and letters
 				tr.appendChild(td);
 				if( i == 0 ) { //number first row
 						td.style.border = "none"; 
+						td.style.cursor = "none";
 						td.textContent = a++;
 				}
 				if( j == 0 ) { // letters first colum
 						td.style.border = "none"; 
+						td.style.cursor = "none";
 						td.textContent = String.fromCharCode(ascii++);
 				}
 				if ( i == 0 && j == 0){ td.textContent = ""; } // corner
@@ -52,37 +54,43 @@ function boardship (){ // create the board with numbers and letters
 }
 
 var x = 0;
-function cellclick (){
-		let celllist = document.getElementsByClassName('cell');    // <------------------------------------------------
-		for (let i = 12 ; i < celllist.length; i++) {
-				celllist[i].addEventListener('click', function (e) { // listen click on board
-						console.log("this is i: " + celllist[i].id);
-						this.classList.add = "black";
-						//celllist[i].style.backgroundColor = "black";
-						if(x < 3){ // first ships options of two cell
-							var cell0 = +(celllist[i].id)[0]; // first at all, take the string and convert to number
-							var cell1 = +(celllist[i].id)[2];
-							if ((celllist[i].id).length == 4 && (celllist[i].id)[1] == ","){cell1 = 10}
-							if ((celllist[i].id).length == 4 && (celllist[i].id)[2] == ","){cell0 = 10; cell1 = +(celllist[i].id)[3];}	
-							if ((celllist[i].id).length == 5 && (celllist[i].id)[2] == ","){cell0 = 10; cell1 = 10}
-							console.log(cell0 + " " + cell1);
-							for (let j = 0 ; j < 5 ; j++) {  // go to "placeships" to enable cells to place each ship cell
-								if (j == 0) { placeships(cell0, cell1)}
-								if (j == 1) { placeships(cell0 + 1, cell1)}
-								if (j == 2) { placeships(cell0 - 1, cell1)}
-								if (j == 3) { placeships(cell0, cell1 + 1)}
-								if (j == 4) { placeships(cell0, cell1 - 1)}
-							}
-						} 
-				});
-		}
+function cellclick (){  // listen clicks on board
+	var celllist = document.getElementsByClassName('cell');
+	for (let i = 12 ; i < celllist.length; i++) {
+		celllist[i].addEventListener('click', function (e) { 
+			console.log("this is i: " + celllist[i].id);
+			removecursor (celllist);
+			var cell0 = +(celllist[i].id)[0];      // first at all, take the string and convert to number
+			var cell1 = +(celllist[i].id)[2];
+			if ((celllist[i].id).length == 4 && (celllist[i].id)[1] == ","){cell1 = 10}
+			if ((celllist[i].id).length == 4 && (celllist[i].id)[2] == ","){cell0 = 10; cell1 = +(celllist[i].id)[3];}	
+			if ((celllist[i].id).length == 5 && (celllist[i].id)[2] == ","){cell0 = 10; cell1 = 10}
+			optionship(cell0, cell1)
+		});
+	}
+}
+
+function optionship (cell0, cell1) {     // go to "placeships" to enable cells to place each ship cell
+	for (let j = 0 ; j < 5 ; j++) {  
+		if (j == 0) { placeships(cell0, cell1)}
+		if (j == 1) { placeships(cell0 + 1, cell1)}
+		if (j == 2) { placeships(cell0 - 1, cell1)}
+		if (j == 3) { placeships(cell0, cell1 + 1)}
+		if (j == 4) { placeships(cell0, cell1 - 1)}
+	}
 }
 
 function placeships(a, b) {
-	console.log(a,b);
-	if (a > 0 && a < 11 && b > 0 && b < 11){ document.getElementById([a,b]).style.backgroundColor = 'red'}
+	if (a > 0 && a < 11 && b > 0 && b < 11){ 
+		document.getElementById([a,b]).style.backgroundColor = 'lightblue'
+		document.getElementById([a,b]).style.cursor = 'pointer'
+	}
+}
+
+function removecursor (celllist) {
+	for (let i = 12 ; i < celllist.length; i++) {celllist[i].style.cursor = "none"}
 }
 // function matrizboard () {
 // }
 
-export { boardship, cellclick };
+export { boardship, cellclick};
